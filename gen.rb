4b0f0de31dc5503926f09e96373d405c22cff20e
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2022-03-20 15:30:24 -0700

    Use Netlify

--- gen.rb
+++ gen.rb
@@ -10,6 +10,20 @@ Dir.glob('*.md') do |path|
     end
 
     File.open(File.join('build', html_path), 'w') do |f|
+        f.write(<<END)
+<meta charset="utf-8"/>
+<style>
+body {
+    font-family: sans-serif;
+}
+.inner {
+    margin: 1rem;
+    max-width: 50rem;
+    line-height: 1.5;
+}
+</style>
+<body><div class="inner">
+END
         f.write(Kramdown::Document.new(File.read(path)).to_html)
     end
 end
