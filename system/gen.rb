56a5049b210a6d86be0614f43e15abc681db1fc7
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2022-03-24 00:01:14 -0700

    Show all pages

--- system/gen.rb
+++ system/gen.rb
@@ -1,31 +1,57 @@
 require 'kramdown'
 require 'fileutils'
+require 'erb'
+
+class Page
+    def initialize(md_path)
+        @md_path = md_path
+        @doc = Kramdown::Document.new(File.read(@md_path))
+        @title = @doc.root.children[0].children[0].value
+    end
+    attr_reader :title
+
+    def html
+        @doc.to_html
+    end
+
+    def html_path
+        path = @md_path.gsub(/\.md$/, '.html')
+        if path == 'README.html'
+            'index.html'
+        else
+            path
+        end
+    end
+end
+
+def generate_file_list(pages, html_path)
+    html = pages.filter do |page|
+        page.html_path != html_path
+    end.map do |page|
+        "<li><a href=#{page.html_path}>#{page.title}</a></li>"
+    end.join("\n")
+    
+    "<ul>#{html}</ul>"
+end
+
+t = ERB.new(File.open('template.html.erb').read)
 
 Dir.chdir(ARGV.shift)
 
 FileUtils.mkdir_p('build')
 
-Dir.glob('*.md') do |path|
-    html_path = path.gsub(/\.md$/, '.html')
-    if html_path == 'README.html'
-        html_path = 'index.html'
-    end
+pages = Dir.glob('*.md').map do |md|
+    Page.new(md)
+end
 
-    File.open(File.join('build', html_path), 'w') do |f|
-        f.write(<<END)
-<meta charset="utf-8"/>
-<style>
-body {
-    font-family: sans-serif;
-}
-.inner {
-    margin: 1rem;
-    max-width: 50rem;
-    line-height: 1.5;
-}
-</style>
-<body><div class="inner">
-END
-        f.write(Kramdown::Document.new(File.read(path)).to_html)
+pages.each do |page|
+    File.open(File.join('build', page.html_path), 'w') do |f|
+        title = page.title
+        body = page.html
+        footer = ''
+        if page.html_path == 'index.html'
+            footer = generate_file_list(pages, page.html_path)
+        end
+        f.write(t.result(binding))
     end
 end
