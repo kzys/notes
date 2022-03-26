e5a6ca22acd4eb96c7e575a474f94106a66f4297
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2022-03-26 01:20:45 -0700

    TOC

--- system/src/main.rs
+++ system/src/main.rs
@@ -1,9 +1,17 @@
 use handlebars::{to_json, Handlebars};
 use itertools::Itertools;
 use pulldown_cmark::{html, Event, HeadingLevel::H1, Parser, Tag};
+use serde::Serialize;
 use serde_json::value::{self, Map, Value as Json};
 use std::error;
 use std::fs;
+
+#[derive(Serialize)]
+struct Page {
+    title: Option<String>,
+    html: String,
+    html_path: String,
+}
 
 fn find_title<'a>(it: impl Iterator<Item = Event<'a>>) -> Option<String> {
     let mut heading = false;
@@ -29,6 +37,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     fs::create_dir_all("build")?;
 
     let paths = fs::read_dir(".")?;
+    let mut pages = Vec::new();
     for path in paths {
         let path = path?;
         if !path.file_name().to_string_lossy().ends_with(".md") {
@@ -52,16 +61,35 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         let (it1, it2) = parser.tee();
         let title = find_title(it1);
 
-        let mut html_output = String::new();
-        html::push_html(&mut html_output, it2);
+        let mut html = String::new();
+        html::push_html(&mut html, it2);
+
+        pages.push(Page {
+            title,
+            html,
+            html_path: html_path.to_string(),
+        });
+    }
+
+    let toc: Vec<&Page> = pages
+        .iter()
+        .sorted_by(|a, b| a.title.as_ref().unwrap().cmp(b.title.as_ref().unwrap()))
+        .filter(|x| x.html_path != "index.html")
+        .collect();
+
+    for page in &pages {
         let mut data = Map::new();
         data.insert(
             "title".to_string(),
-            to_json(title.or(Some("Untitled".to_string()))),
+            to_json(page.title.as_ref().or(Some(&"Untitled".to_string()))),
         );
-        data.insert("main".to_string(), to_json(html_output));
+        data.insert("page".to_string(), to_json(&page));
 
-        let dest = format!("build/{}", html_path);
+        if page.html_path == "index.html" {
+            data.insert("pages".to_string(), to_json(&toc));
+        }
+
+        let dest = format!("build/{}", page.html_path);
         let f = fs::File::create(dest)?;
         reg.render_to_write("tp", &data, f)?;
     }
