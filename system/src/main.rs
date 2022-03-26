3e0dc7a040463e49e36acd75517ef7fa775d6fe2
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2022-03-26 00:27:18 -0700

    Do not make Parser twice

--- system/src/main.rs
+++ system/src/main.rs
@@ -1,8 +1,25 @@
 use handlebars::{to_json, Handlebars};
+use itertools::Itertools;
 use pulldown_cmark::{html, Event, HeadingLevel::H1, Parser, Tag};
 use serde_json::value::{self, Map, Value as Json};
 use std::error;
 use std::fs;
+
+fn find_title<'a>(it: impl Iterator<Item = Event<'a>>) -> Option<String> {
+    let mut heading = false;
+    let mut title = None;
+    it.for_each(|ev| match ev {
+        Event::Start(Tag::Heading(H1, _, _)) => heading = true,
+        Event::End(Tag::Heading(H1, _, _)) => heading = false,
+        Event::Text(text) => {
+            if heading {
+                title = Some(text.to_string())
+            }
+        }
+        _ => {}
+    });
+    title
+}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let mut reg = Handlebars::new();
@@ -32,21 +49,11 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         let content = fs::read_to_string(path.path())?;
         let parser = Parser::new(&content);
 
-        let mut heading = false;
-        let mut title = None;
-        parser.for_each(|ev| match ev {
-            Event::Start(Tag::Heading(H1, _, _)) => heading = true,
-            Event::End(Tag::Heading(H1, _, _)) => heading = false,
-            Event::Text(text) => {
-                if heading {
-                    title = Some(text.to_string())
-                }
-            }
-            _ => {}
-        });
-        let parser = Parser::new(&content);
+        let (it1, it2) = parser.tee();
+        let title = find_title(it1);
+
         let mut html_output = String::new();
-        html::push_html(&mut html_output, parser);
+        html::push_html(&mut html_output, it2);
         let mut data = Map::new();
         data.insert(
             "title".to_string(),
