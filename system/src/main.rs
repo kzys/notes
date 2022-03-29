2ffe1c33d7d01b044179a6ae9473a2b71dbb1b83
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2022-03-29 08:35:14 -0700

    Recursively convert all files

--- system/src/main.rs
+++ system/src/main.rs
@@ -6,12 +6,13 @@ use serde::Serialize;
 use serde_json::value;
 use std::error;
 use std::fs;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::str::FromStr;
 
 #[derive(Serialize)]
 struct Page {
-    title: Option<String>,
+    title: String,
     html: String,
     html_path: String,
     size: u64,
@@ -21,11 +22,19 @@ struct Page {
 }
 
 impl Page {
-    fn created_at(&self) -> DateTime<Utc> {
-        Utc.timestamp(self.changes[self.changes.len() - 1] as i64, 0)
+    fn created_at(&self) -> Option<DateTime<Utc>> {
+        if self.changes.len() == 0 {
+            None
+        } else {
+            Some(Utc.timestamp(self.changes[self.changes.len() - 1] as i64, 0))
+        }
     }
-    fn last_modified_at(&self) -> DateTime<Utc> {
-        Utc.timestamp(self.changes[0] as i64, 0)
+    fn last_modified_at(&self) -> Option<DateTime<Utc>> {
+        if self.changes.len() == 0 {
+            None
+        } else {
+            Some(Utc.timestamp(self.changes[0] as i64, 0))
+        }
     }
 }
 
@@ -43,6 +52,82 @@ fn find_title<'a>(it: impl Iterator<Item = Event<'a>>) -> Option<String> {
         _ => {}
     });
     title
+}
+
+fn find_files(from: &Path, ext: &str) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let mut result: Vec<PathBuf> = vec![];
+    let paths = fs::read_dir(from)?;
+    for path in paths {
+        let path = path?;
+        let mut pb = PathBuf::from(&path.file_name());
+        if from.as_os_str() != "." {
+            pb = from.join(pb)
+        }
+        if let Ok(t) = path.file_type() {
+            if t.is_dir() {
+                result.append(&mut find_files(&pb, ext)?)
+            }
+
+            if let Some(e) = pb.extension() {
+                if e == ext {
+                    result.push(pb);
+                }
+            }
+        }
+    }
+    Ok(result)
+}
+
+fn collect_pages(
+    dir: &str,
+    files: &std::collections::HashMap<String, Vec<u64>>,
+) -> Result<Vec<Page>, Box<dyn std::error::Error>> {
+    let dir = Path::new(dir);
+    let paths = find_files(dir, "md")?;
+    let mut pages = Vec::new();
+    for path in paths {
+        let converted_path = path.to_str().unwrap().replace(".md", ".html");
+
+        let p = if path.ends_with("README.md") {
+            "index.html"
+        } else {
+            &converted_path
+        };
+
+        let html_path = if dir.as_os_str() != "." {
+            PathBuf::from(p)
+        } else {
+            dir.join(p)
+        };
+
+        let content = fs::read_to_string(&path)?;
+        let parser = Parser::new(&content);
+
+        let (it1, it2) = parser.tee();
+        let title = find_title(it1);
+
+        let mut html = String::new();
+        html::push_html(&mut html, it2);
+
+        let size = fs::metadata(&path)?.len();
+        let empty = vec![];
+        let k = path.to_str().unwrap().to_string();
+        let changes = files.get(&k).unwrap_or(&empty);
+
+        let mut p = Page {
+            title: title.unwrap_or_else(|| path.to_str().unwrap().to_string().clone()),
+            html,
+            html_path: html_path.to_string_lossy().to_string(),
+            size,
+            changes: changes.to_vec(),
+            created_at: None,
+            last_modified_at: None,
+        };
+        p.created_at = p.created_at().map(|x| x.to_string());
+        p.last_modified_at = p.last_modified_at().map(|x| x.to_string());
+        pages.push(p);
+    }
+    Ok(pages)
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
@@ -67,85 +152,40 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         if columns.len() > 2 && columns[0] == "commit" {
             dt = Some(u64::from_str(columns[2])?);
         } else if columns.len() > 2 {
-            let key = columns[2].to_string();
+            let mut key = columns[2].to_string();
+            let rename: Vec<&str> = key.split(" => ").collect();
+            if rename.len() == 2 {
+                key = rename[1].to_string();
+            }
             files.entry(key).or_insert(vec![]).push(dt.unwrap());
         } else {
             dt = None;
         }
     }
 
-    let paths = fs::read_dir(".")?;
-    let mut pages = Vec::new();
-    for path in paths {
-        let path = path?;
-        if !path.file_name().to_string_lossy().ends_with(".md") {
-            continue;
-        }
-
-        let converted_path = path
-            .file_name()
-            .to_str()
-            .ok_or("err")?
-            .replace(".md", ".html");
-        let html_path = if converted_path == "README.html" {
-            "index.html"
-        } else {
-            &converted_path
-        };
-
-        let content = fs::read_to_string(path.path())?;
-        let parser = Parser::new(&content);
-
-        let (it1, it2) = parser.tee();
-        let title = find_title(it1);
-
-        let mut html = String::new();
-        html::push_html(&mut html, it2);
-
-        let size = fs::metadata(path.path())?.len();
-        let empty = vec![];
-        let changes = files
-            .get(&path.file_name().to_string_lossy().to_string())
-            .unwrap_or(&empty);
-
-        let mut p = Page {
-            title,
-            html,
-            html_path: html_path.to_string(),
-            size,
-            changes: changes.to_vec(),
-            created_at: None,
-            last_modified_at: None,
-        };
-        p.created_at = Some(p.created_at().to_string());
-        p.last_modified_at = Some(p.last_modified_at().to_string());
-        pages.push(p);
-    }
+    let pages: Vec<Page> = collect_pages(".", &files)?;
 
     let toc: Vec<&Page> = pages
         .iter()
-        .sorted_by(|a, b| a.title.as_ref().unwrap().cmp(b.title.as_ref().unwrap()))
+        .sorted_by(|a, b| a.title.cmp(&b.title))
         .filter(|x| x.html_path != "index.html")
         .collect();
 
     for page in &pages {
         let mut data = value::Map::new();
-        data.insert(
-            "title".to_string(),
-            to_json(page.title.as_ref().or(Some(&"Untitled".to_string()))),
-        );
+        data.insert("title".to_string(), to_json(&page.title));
         data.insert("size".to_string(), to_json(page.size));
         data.insert("page".to_string(), to_json(&page));
-        data.insert(
-            "last_modified_at".to_string(),
-            to_json(page.last_modified_at().to_string()),
-        );
 
-        if page.html_path == "index.html" {
+        if page.html_path.ends_with("index.html") {
             data.insert("pages".to_string(), to_json(&toc));
         }
 
-        let dest = format!("build/{}", page.html_path);
+        let s = format!("build/{}", page.html_path);
+        let dest = std::path::Path::new(&s);
+        if let Some(p) = dest.parent() {
+            fs::create_dir_all(p)?;
+        }
         let f = fs::File::create(dest)?;
         reg.render_to_write("tp", &data, f)?;
     }
